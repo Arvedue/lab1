3a2e85802bed52ab20aa52693f7c03c1e897e49b
Arvedue <janchandaev@gmail.com>
2023-02-13 21:56:56 +0600

    Add project files.

---
+++ lab1/Exercises.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Exercises
+    {
+        public static string getArray(int[] arr, int n) {
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < n - 1; i++)
+                res.Append(arr[i] + ", ");
+            res.Append(arr[n - 1]);
+
+            return res.ToString();
+        }
+
+        public static string exercise1(int[] arr, int n)
+        {
+            StringBuilder res = new StringBuilder();
+            int max = arr[0];
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                    index = i;
+                }
+            }
+
+            if (index != 0 || index != n - 1)
+            {
+                double sum = 0;
+                double product = 1;
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (i < index) product *= arr[i];
+                    else if (i > index) sum += arr[i];
+                }
+
+                res.Append("Product of elements before Max: " + product +
+                    "\nSum of elements after Max: " + sum +
+                    "\nResult: " + product / sum);
+            }
+            else res.Append("Can't find result! The max is the first or last element in the array.");
+
+            return res.ToString();
+        }
+
+        public static string exercise2(int[] arr, int n)
+        {
+            StringBuilder res = new StringBuilder();
+            int index = -1;
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (arr[i] > 0 && arr[i + 1] > 0)
+                    if (arr[i] == arr[i + 1]) index = i;
+            }
+
+            if (index != -1)
+            {
+                res.Append("Deleted element: " + arr[index]);
+                for (int i = index; i < n - 2; i++)
+                    arr[i] = arr[i + 2];
+
+                res.Append("\nModified Array: \n");
+                res.Append(getArray(arr, n - 2));
+            }
+            else res.Append("There're no 2 consecutive positive elements in the array!");
+
+            return res.ToString();
+        }
+
+        public static string exercise3(int[] arr, int n, int num)
+        {
+            StringBuilder res = new StringBuilder();
+
+            int index = -1;
+            for (int i = 0; i < n; i++)
+                if (arr[i] < 0) index = i;
+
+            if (index != -1)
+            {
+                n += 2;
+                index += 2;
+                for (int i = n; i >= index; i--)
+                    arr[i] = arr[i - 2];
+                arr[index - 1] = num;
+                arr[index] = num;
+
+
+                res.Append("\nModified Array: \n");
+                res.Append(getArray(arr, n));
+            }
+            else res.Append("There's no negative numbers in the array.");
+
+            return res.ToString();
+        }
+
+        public static string exercise4(int[] arr, int n)
+        {
+            StringBuilder res = new StringBuilder();
+            bool contains = false;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    contains = true;
+                    break;
+                }
+            }
+
+            if (contains) res.Append("There's positive numbers in the array.");
+            else res.Append("There's NO positive numbers in the array.");
+
+            return res.ToString();
+        }
+    }
+}
---
+++ lab1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int[] arr;
+        private int n;
+
+        private void initialize()
+        {
+            arr = new int[11];
+            arr[0] = 2;
+            arr[1] = 2;
+            arr[2] = 3;
+            arr[3] = 4;
+            arr[4] = 1;
+            arr[5] = 1;
+            arr[6] = -1;
+            arr[7] = 2;
+
+            n = 8;
+
+            lblArray.Text = "Array elements: " + Exercises.getArray(arr, n);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            initialize();
+            lblResult.Text = Exercises.exercise1(arr, n);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            initialize();
+            lblResult.Text = Exercises.exercise2(arr, n);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            initialize();
+            if (txtNum.Text != "")
+            {
+                int num = Convert.ToInt32(txtNum.Text);
+                lblResult.Text = Exercises.exercise3(arr, n, num);
+            }
+            else lblResult.Text = "Input value is incorrect!";
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            initialize();
+            lblResult.Text = Exercises.exercise4(arr, n);
+        }
+    }
+}
